54a2421e1cce4192acccc66415b8738a4f8589e2
lovedbfkdbfk-coder <lovedbfkdbfk@gmail.com>
2026-07-10 08:03:13 +0000

    Firebase 연결 수정 전 백업

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Firebase SDK 불러오기
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
+
+// Firebase 설정
+const firebaseConfig = {
+  apiKey: "AIzaSyDMTPOkpKYJBi-6yQn9DQIksXbRL6GzKrA",
+  authDomain: "fukuoka-6315e.firebaseapp.com",
+  databaseURL: "https://fukuoka-6315e-default-rtdb.firebaseio.com",
+  projectId: "fukuoka-6315e",
+  storageBucket: "fukuoka-6315e.firebasestorage.app",
+  messagingSenderId: "566795703366",
+  appId: "1:566795703366:web:5dcb3ff8d1939e723f2647"
+};
+
+// Firebase 시작
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// 다른 파일에서도 사용할 수 있도록 내보내기
+export { database };
+
+console.log("✅ Firebase 연결 성공!");
